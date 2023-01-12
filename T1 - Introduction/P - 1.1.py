eb99ce147b33617e0e0cc7c1084111547a549a0f
diegonti <diegontidoc2000@gmail.com>
2023-01-12 16:02:11 +0100

    Updated docs and header

--- T1 - Introduction/P - 1.1.py
+++ T1 - Introduction/P - 1.1.py
@@ -1,19 +1,24 @@
+"""
+Problem 1.1 - Uniform Distribution
+Visualization of the uniform distribution with histogram.
+Diego Ontiveros
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-n = 10000                                   #Number of random numbers generated
-n_bins = 20                                 #Number of bins (boxes) in the histogram
-random_numbers = np.random.uniform(size=n)  #Array with the random numbers
+n = 10000                                   # Number of random numbers generated
+n_bins = 20                                 # Number of bins (boxes) in the histogram
+random_numbers = np.random.uniform(size=n)  # Array with the random numbers
 
-mean = np.mean(random_numbers)              #Average of the random numbers
+mean = np.mean(random_numbers)              # Average of the random numbers
 print(f"Average of the {n} random numbers: {mean:.3f}")
 text = f"Mean = {mean:.3f}"
 
-#Plotting and Plot settings
+# Plotting and Plot settings
 plt.hist(random_numbers,rwidth=0.95,density=True,bins=n_bins,color="teal")
-plt.axhline(1, color = "red")                       #density=True returns the probability density 
-plt.xlim(0,1)                                       #calculated with counts/(sum(counts)*bin.width)
-plt.xlabel("Random value")                          #such that the area under the histogram is 1.
+plt.axhline(1, color = "red")                       # density=True returns the probability density 
+plt.xlim(0,1)                                       # calculated with counts/(sum(counts)*bin.width)
+plt.xlabel("Random value")                          # such that the area under the histogram is 1.
 plt.ylabel("Probability density")
 props = dict(boxstyle='round', facecolor='wheat', alpha=0.85)
 plt.text(0.05, 0.15, text,verticalalignment='top', bbox=props)
