5bdb155523afb45840d01b44c478413b5782d344
diegonti <diegontidoc2000@gmail.com>
2023-01-12 18:59:44 +0100

    Updated docs and header

--- T3.4 - Interpolation/P - 3.4.1.py
+++ T3.4 - Interpolation/P - 3.4.1.py
@@ -1,4 +1,8 @@
-### Exercise 3.4.1
+"""
+Problem 3.4.1 - Interpolation.
+Interpolating function from datapoints.
+Diego
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -6,21 +10,21 @@ import matplotlib.pyplot as plt
 def interpolate(xPoints,yPoints,x):
     """Interpolates x (number or array) linearly to a data table"""
 
-    #If x is number
+    # If x is number
     if type(x) == int or type(x) == float: 
         for i,p in enumerate(yPoints):
-            try: #Try-Except in case the index is out of bounds (for the las number)
-                #get the table points interval
+            try: # Try-Except in case the index is out of bounds (for the las number)
+                # Get the table points interval
                 x1,x2 = xPoints[i],xPoints[i+1]
                 fx1,fx2 = yPoints[i],yPoints[i+1]
                 
-                #Calculate the linear interpolation in the interval
+                # Calculate the linear interpolation in the interval
                 if x >= xPoints[i] and x < xPoints[i+1]:
                     fx = fx1 + (x-x1)*(fx2-fx1)/(x2-x1)
                     return fx
             except IndexError: pass
 
-    #If x is an iterable (array)        
+    # If x is an iterable (array)        
     else: 
         fx = np.array([])
         for j in x:
@@ -36,19 +40,19 @@ def interpolate(xPoints,yPoints,x):
                 except IndexError: pass
         return fx
 
-##Main Program
+###################  Main Program  #################
 def f(x): return np.cos(x)
 
-#Original Function
+# Original Function
 x100 = np.linspace(0,10, num = 100)
 x1000 = np.linspace(0,10, num = 1000)
 fx = f(x1000)
 
-#Table Points
+# Table Points
 xPoints = np.linspace(0,10,20)
 yPoints = f(xPoints) 
 
-#Plot Settings
+# Plot Settings
 plt.plot(x1000,fx)
 plt.plot(xPoints,yPoints,"x")
 plt.plot(x100,interpolate(xPoints,yPoints,x100))
--- T3.4 - Interpolation/P - 3.4.2.py
+++ T3.4 - Interpolation/P - 3.4.2.py
@@ -1,4 +1,8 @@
-### Exercise 3.4.2
+"""
+Problem 3.4.1 - Interpolation.
+Interpolating function from datapoints with Lagrange method.
+Diego
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -16,24 +20,24 @@ def interpolate(xPoints,yPoints,x):
         p += lagrange(x,xi,xPoints) * yPoints[i]
     return p
 
-##Main Program
+###################  Main Program  #################
 def f(x): return np.cos(x)
 
-#Number o points and step
-n, h = 20,0.1 #n+1 points --> n degree
-x = np.arange(0,n*h+h,h)                #For interpolation
-xSmooth = np.linspace(0,n*h+h,1000)     #For representing cos
+# Number o points and step
+n, h = 20,0.1                           # n+1 points --> n degree
+x = np.arange(0,n*h+h,h)                # For interpolation
+xSmooth = np.linspace(0,n*h+h,1000)     # For representing cos
 fx = f(xSmooth)
 
-#Table points (grid)
+# Table points (grid)
 xPoints = np.linspace(0,n*h+h,n)
 yPoints = f(xPoints)
 
-#Interpolation
+# Interpolation
 yInterpolate = []
 for xi in x: yInterpolate.append(interpolate(xPoints,yPoints,xi))
 
-#Plot Settings
+# Plot Settings
 plt.plot(xSmooth,fx)
 plt.plot(xPoints,yPoints, "x")
 plt.plot(x,yInterpolate)
@@ -41,5 +45,5 @@ plt.plot(x,yInterpolate)
 plt.legend(["cos(x)", "table points", "interpolation"])
 plt.show()
 
-#One can see that the Lagrange polynomial interpolation does a good job of recreating the function at
-#higher n. Nevertheless, at very high n's (<70) the interpolation starts to break.
+# One can see that the Lagrange polynomial interpolation does a good job of recreating the function at
+# higher n. Nevertheless, at very high n's (<70) the interpolation starts to break.
